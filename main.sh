2daa646723cafea799e3379c396db1bb797355a4
Ms-Lina <linamis@gmail.com>
2023-10-04 10:02:21 -0700

    Display the list

--- main.sh
+++ main.sh
@@ -4,3 +4,7 @@
 # Prompt for student information
    read -p "Enter student email: " email            read -p "Enter student age: " age            read -p "Enter student ID: " student_id             # Append student information to the data file
    echo "$email,$age,$student_id" >> students-list_0923.txt            echo "Student record created successfully!"   
+ # Display the list of all students from the data file
+ echo "List of Students:"  
+ cat students-list_0923.txt  
+
