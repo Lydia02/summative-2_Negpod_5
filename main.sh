25689574c6ebd7b8c5efee2013291984f6b00cb9
jasingizwe <c.asingizwe@alustudent.com>
2023-10-04 02:07:27 -0700

    I did it!

---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Use a case statement to handle user's choice
+    case $choice in        
+# Prompt for student information
+   read -p "Enter student email: " email            read -p "Enter student age: " age            read -p "Enter student ID: " student_id             # Append student information to the data file
+   echo "$email,$age,$student_id" >> students-list_0923.txt            echo "Student record created successfully!"   
