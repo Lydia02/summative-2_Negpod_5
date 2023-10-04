54bb928095029fc2c77053e334094a0eda4409d7
Maridadi Thierry <t.maridadi@alustudent.com>
2023-10-04 09:50:11 -0700

    exit the application

--- main.sh
+++ main.sh
@@ -3,4 +3,5 @@
     case $choice in        
 # Prompt for student information
    read -p "Enter student email: " email            read -p "Enter student age: " age            read -p "Enter student ID: " student_id             # Append student information to the data file
-   echo "$email,$age,$student_id" >> students-list_0923.txt            echo "Student record created successfully!"   
+   echo "$email,$age,$student_id" >> students-list_0923.txt            echo "Student record created successfully!"  
+# Exit the application            echo "Exiting the application."            exit 0            ;;        *)            # Handle invalid choices            echo "Invalid choice! Please select a valid option."            ;;    esac    # Pause and wait for user input before continuing    read -p "Press Enter to continue..."done 
