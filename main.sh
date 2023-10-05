7ee419b451e5e5d4449e1cdb82c3df17e5dfcc5a
Ojoawo Lydia Subuola <ojoawolydia@gmail.com>
2023-10-05 19:02:09 +0100

    Resolve Merge conflict

--- main.sh
+++ main.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-# Use a case statement to handle user's choice
-    case $choice in        
-# Prompt for student information
-   read -p "Enter student email: " email            read -p "Enter student age: " age            read -p "Enter student ID: " student_id             # Append student information to the data file
-   echo "$email,$age,$student_id" >> students-list_0923.txt            echo "Student record created successfully!"   
-# Display the list of all students from the data file
- echo "List of Students:"
- cat students-list_0923.txt
-=======
->>>>>>> ba66fdfbe072ac72dfb1aafce5f09f19f1eaf26a
 
 # Start an infinite loop for the student registration system
 while true; do
@@ -21,7 +10,7 @@ while true; do
     echo "4. Update Student Record"
     echo "5. Exit"
     
-    #Next Prompt the user to enter their choice
+    # Prompt the user to enter their choice
     read -p "Enter your choice (1/2/3/4/5): " choice
 
     # Use a case statement to handle user's choice
