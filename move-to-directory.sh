ecaaf2d0aae127c300effd8bff44de0f872133f5
Lydia02 <ojoawolydia@gmai.com>
2023-10-06 15:56:05 -0700

    question 2 refactored

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,1 +1,13 @@
-#!/bin/bash# Check if the target directory exists, if not, create ittarget_dir="negpod_id-q1"if [ ! -d "$target_dir" ]; then    mkdir "$target_dir"fi# Move all four files created in Question 1 to the target directorymv main.sh Students-list_0923.txt select-emails.sh student-emails.txt "$target_dir"echo "Files moved to $target_dir successfully!"
+#!/bin/bash
+
+# Check if the target directory exists, if not, create it
+target_dir="negpod_id-q1"
+if [ ! -d "$target_dir" ]; then
+    mkdir "$target_dir"
+fi
+
+# Move all four files created in Question 1 to the target directory
+mv main.sh Students-list_0923.txt select-emails.sh student-emails.txt "$target_dir"
+
+echo "Files moved to $target_dir successfully!"
+
