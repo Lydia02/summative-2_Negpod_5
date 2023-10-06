907a7d5be24ae95215dd3258332d3b0b7bbec2f9
Maridadi Thierry <t.maridadi@alustudent.com>
2023-10-06 09:51:55 -0700

    new file

---
+++ move-to-directory.sh
@@ -0,0 +1,1 @@
+#!/bin/bash# Check if the target directory exists, if not, create ittarget_dir="negpod_id-q1"if [ ! -d "$target_dir" ]; then    mkdir "$target_dir"fi# Move all four files created in Question 1 to the target directorymv main.sh Students-list_0923.txt select-emails.sh student-emails.txt "$target_dir"echo "Files moved to $target_dir successfully!"
